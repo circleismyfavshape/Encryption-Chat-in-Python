b39b43327b9266d17b5b69dea1ba19948d207404
circleismyfavshape <ahmedthestupid@gmail.com>
2026-05-16 22:30:53 +0800

    simplify chat by removing RSA encryption and hardcoding IP
    Remove RSA key exchange and hardcode server IP for easier local testing.

--- main.py
+++ main.py
@@ -12,29 +12,21 @@ if choice == "1":
     server.listen()
 
     client, _ = server.accept()
-    client.send(pub.save_pkcs1())
-    partner_pub_pem = client.recv(1024)
-    partner_pub = rsa.PublicKey.load_pkcs1(partner_pub_pem)
 elif choice == "2":
-    pub, priv = rsa.newkeys(512)
-    ip = input("Enter server IP: ")
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((ip, 9999))
-    partner_pub_pem = client.recv(1024)
-    partner_pub = rsa.PublicKey.load_pkcs1(partner_pub_pem)
-    client.send(pub.save_pkcs1())
+    client.connect(("192.168.56.1", 9999))
 else: 
     exit()
  
 def sending_messages(c):
     while True:
         message = input("")
-        c.send (message.encode)
-        print("You: "+message)
+        c.send (message.encode())
+        print("You: "+ message)
 
 def receiving_messages(c):
     while True:
         print("Partner: "+c.recv(1024).decode())
 
-threading.Thread(target=sending_messages, args=(client))
-threading.Thread(target=receiving_messages, args=(client))
+threading.Thread(target=sending_messages, args=(client,)).start()
+threading.Thread(target=receiving_messages, args=(client,)).start()
