36c501a12097ba4a88e40d2d6a76b7f2ce9a926a
circleismyfavshape <ahmedthestupid@gmail.com>
2026-05-17 10:08:56 +0800

    implement end-to-end RSA encryption for all messages
    Encrypt outgoing messages with partner's public key and decrypt incoming
    messages with local private key, ensuring plaintext is never sent over the socket.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import socket
 import threading
 
-import rsa 
+import rsa
 
+public_key, private_key = rsa.newkeys(1024)
+public_partner = None
 
 choice = input("Do you want to host (1) or to connect (2): ")
 
@@ -12,21 +14,25 @@ if choice == "1":
     server.listen()
 
     client, _ = server.accept()
+    client.send(public_key.save_pkcs1("PEM"))
+    public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
 elif choice == "2":
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(("192.168.56.1", 9999))
+    client.send(public_key.save_pkcs1("PEM"))
+    public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
 else: 
     exit()
  
 def sending_messages(c):
     while True:
         message = input("")
-        c.send (message.encode())
+        c.send (rsa.encrypt(message.encode(), public_partner))
         print("You: "+ message)
 
 def receiving_messages(c):
     while True:
-        print("Partner: "+c.recv(1024).decode())
+        print("Partner: " + rsa.decrypt(c.recv(1024), private_key).decode())
 
 threading.Thread(target=sending_messages, args=(client,)).start()
 threading.Thread(target=receiving_messages, args=(client,)).start()
